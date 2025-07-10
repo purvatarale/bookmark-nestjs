90c8c7808542cbdf65b67aaa0a4cf2d2e23fc21f
Purva <20004113@iitb.ac.in>
2025-07-10 17:08:54 +0530

    docker and prisma setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post } from "@nestjs/common"
+import { Body, Controller, Post } from "@nestjs/common"
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authservice:AuthService) {}
     @Post('signup')
-    signup(){
+    signup(@Body() dto:any){
+        console.log({
+            dto,
+        });
         return this.authservice.signup()
     }
     @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@nestjs/common";
+// import { Prisma } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
-@Injectable({})
+@Injectable()
 export class AuthService{
+    constructor(private prisma: PrismaService){}
     signup(){
         return { msg: 'I have signed up'};
     }

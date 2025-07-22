8ca4f299726e387f747702188a44c18ea12d4db2
Purva <20004113@iitb.ac.in>
2025-07-22 17:48:51 +0530

    passport authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,29 +5,34 @@ import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 // import { PrismaClientKnownRequestError } from "generated/prisma/runtime/library";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { promises } from "dns";
+import { access } from "fs";
 
 
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaService){}
-    async signup(dto:AuthDto){
+export class AuthService {
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) { }
+    async signup(dto: AuthDto) {
         //generate hashed pass
         const hash = await argon.hash(dto.password);
 
         //save new user in db
-        try{
+        try {
             const user = await this.prisma.user.create({
-                data:{
-                    email:dto.email,
+                data: {
+                    email: dto.email,
                     hash
                 }
             });
-    
-            const { hash: _, ...userWithoutHash } = user;
-            return userWithoutHash;
-        }catch(error){
-            if ( error instanceof PrismaClientKnownRequestError){
-                if ( error.code === 'P2002'){
+
+            // const { hash: _, ...userWithoutHash } = user;
+            // return userWithoutHash;
+            return this.signToken(user.id, user.email);
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
                     throw new ForbiddenException("Credentials Taken")
                 }
             }
@@ -39,10 +44,10 @@ export class AuthService{
         // return user;
         // return { msg: 'I have signed up'};
     }
-    async signin(dto:AuthDto){
+    async signin(dto: AuthDto) {
         //find the user by id
         const user = await this.prisma.user.findUnique({
-            where:{
+            where: {
                 email: dto.email
             }
         })
@@ -56,8 +61,26 @@ export class AuthService{
         if (!pwMatches)
             throw new ForbiddenException("Incorrect Password")
         //return user
-        const { hash: _, ...userWithoutHash } = user;
-        return userWithoutHash;
+        // const { hash: _, ...userWithoutHash } = user;
+        // return userWithoutHash;
         // return{ msg: 'I have signed in'};
+        return this.signToken(user.id, user.email);
+    }
+    async signToken(
+        userId: number,
+        email: string,
+    ): Promise<{access_token:string}> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET')
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: secret,
+        })
+        return{
+            access_token:token
+        }
     }
 }
